cbc4897db24983a37992924367f69f2202b671ca
ajays <ajay_sieunarine@hotmail.com>
2022-07-06 09:42:09 -0400

    add android app and link to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:ig_clone/responsive/mobile_screen_layout.dart';
 import 'package:ig_clone/responsive/responsive_layout_screen.dart';
@@ -7,7 +8,21 @@ import 'package:ig_clone/utils/colors.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyAV63d9iHM_Jp06CAj8oPy0aA2eOYSu5IQ",
+        authDomain: "instagram-clone-f0f5e.firebaseapp.com",
+        projectId: "instagram-clone-f0f5e",
+        storageBucket: "instagram-clone-f0f5e.appspot.com",
+        messagingSenderId: "907235225151",
+        appId: "1:907235225151:web:6dcab2a95b4d1ab465928a",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
@@ -23,7 +38,8 @@ class MyApp extends StatelessWidget {
         theme: ThemeData.dark().copyWith(
           scaffoldBackgroundColor: mobileBackgroundColor,
         ),
-        home: const ResponsiveLayout( // const since no dynamic values used in constructors
+        home: const ResponsiveLayout(
+          // const since no dynamic values used in constructors
           mobileScreenLayout: MobileScreenLayout(),
           webScreenLayout: WebScreenLayout(),
         ));
